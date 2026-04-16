8cfed18427edda500bfed13fc13929358698c550
Whadpoind04 <vinicius.assis.costa01@gmail.com>
2026-04-16 02:13:11 -0300

    Classe Processo

---
+++ Processo.java
@@ -0,0 +1,56 @@
+public class Processo {
+    int cont = 1;
+    private int protocolo;
+    private String solicitante;
+    private String tipoServico;
+    private int prioridade;
+    private String dataHora;
+
+    public void Protocolo (String solicitante, String tipoServico, int prioridade){
+        this.protocolo = cont++;
+        this.solicitante = solicitante;
+        this.tipoServico = tipoServico;
+        this.prioridade = prioridade;
+        this.dataHora = dataHora;
+    }
+
+    public int getProtocolo(){
+        return protocolo;
+    } 
+    public String getSolicitante(){
+        return solicitante;
+    }
+    public String getTipoServico(){
+        return tipoServico;
+    }
+    public int getPrioridade(){
+        return prioridade;
+    }
+    public String getDataHora(){
+        return dataHora;
+    }
+
+    public void setSolicitante(String solicitante){
+        this.solicitante = solicitante;
+    }
+    public void setTipoServico (String tipoServico){
+        this.tipoServico = tipoServico;
+    }
+    public void setPrioridade (int prioridade){
+        this.prioridade = prioridade;
+    }
+    public void setDataHora (String dataHora){
+        this.dataHora = dataHora;
+    }
+    @Override
+    public String toString(){
+        String nivel =switch (prioridade){
+        case 1 -> "Baixa";
+        case 2 -> "Normal";
+        case 3 -> "Urgente";
+        default -> "Desconhecida";
+        };
+        return "["+protocolo+"]" +"["+solicitante +"]"  + "[" +tipoServico + "]" + "[" +nivel +"]"+ "["+dataHora +"]" ;
+    }
+
+}
